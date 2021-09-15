b1e9044b16597aa7547ee84cfa219f87bf733458
RoyceFu <royce.code@gmail.com>
2021-09-15 07:56:48 -0400

    royce - commit

--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -3,13 +3,33 @@ package org.xpdojo.bank;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.is;
+
 
 public class AccountTest {
 
     @Test
-	@Disabled
+    public void startingBalanceIsZero() {
+        Account account = new Account();
+        assertThat(account.balance, is(0));
+    }
+
+    @Test
     public void depositAnAmountToIncreaseTheBalance() {
+
         assertThat("your first test").isBlank();
+        // arrange
+        Account account = new Account();
+        account.deposit(100);
+        assertThat(account.balance, is(100));
+    }
+
+    @Test
+    public void depostingMultipleAmounts() {
+        Account account = new Account();
+        account.deposit(100);
+        account.deposit(300);
+        assertThat(account.balance, is(400));
     }
 }
